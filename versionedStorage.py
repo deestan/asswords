d6a65e1dfc40a98a33ad76ecf84cf930f5367382
Helge Skogly Holm <helge.holm@gmail.com>
2013-06-09 21:10:22 +0200

    Corrected git usage on pull.

--- versionedStorage.py
+++ versionedStorage.py
@@ -28,8 +28,11 @@ class VersionedStorage:
         self.commit("Deleted entry %s"%name)
 
     def pull(self):
-        call([ "git", "pull", "-q", "--strategy=recursive", "--strategy-option=ours",
-               "origin", "master" ], cwd=self.dbDir)
+        call([ "git", "fetch", "-q", "origin", "master", cwd=self.dbDir)
+        call([ "git", "merge", "-q",
+               "--strategy=recursive",
+               "--strategy-option=ours",
+               "origin/master" ], cwd=self.dbDir)
 
     def push(self):
         self.commit("Pushing offline changes.")
